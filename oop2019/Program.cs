0bab8cb9216533ae99657f69332166a8ae08c437
Henrik Bartholdsson <henrik.bartholdsson@hotmail.com>
2019-11-12 13:30:58 +0100

    Add project files.

---
+++ oop2019/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
